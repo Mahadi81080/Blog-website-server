da93a482d2918d5b2dd2e013b8c7808447490870
Mahadi810803 <mahedihasan81080@gmail.com>
2024-05-11 00:30:00 +0600

    jwt function implement

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+var cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://blog-website-e15da.web.app",
+      "https://blog-website-e15da.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ityl5rk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -18,7 +30,30 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-
+// middleware
+const logger = async (req, res, next) => {
+  console.log("called", req.host, req.originalUrl);
+  next();
+};
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log("value of tokoen in middleware", token);
+  if (!token) {
+    return res.status(401).send({ message: "unauthorize token" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorize access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+const cookeOption = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ? true : false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -26,19 +61,38 @@ async function run() {
     const blogCollection = client.db("blogDB").collection("blog");
     const commentCollection = client.db("blogDB").collection("comment");
 
-    app.get("/addBlog", async (req, res) => {
+    // Auth related api
+
+    app.post("/jwt", logger, async (req, res) => {
+      const user = req.body;
+      console.log("user of token", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.cookie("token", token, cookeOption).send({ success: true });
+    });
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("logout", user);
+      res
+        .clearCookie("token", { ...cookeOption, maxAge: 0 })
+        .send({ success: true });
+    });
+
+    // Blog related Api
+    app.get("/addBlog", logger, async (req, res) => {
       const cursor = blogCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.get("/addBlog/:id", async (req, res) => {
+    app.get("/addBlog/:id", logger, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await blogCollection.findOne(query)
-      res.send(result)
+      const result = await blogCollection.findOne(query);
+      res.send(result);
     });
 
-    app.post("/addBlog", async (req, res) => {
+    app.post("/addBlog", logger, async (req, res) => {
       const newBlog = req.body;
       console.log(newBlog);
       const result = await blogCollection.insertOne(newBlog);
